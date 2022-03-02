868a007c9bf170c427a578316a7f8983bd6530fb
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-02 22:15:27 +0100

    app use router added

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,9 @@
-
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 const { notFoundError, generalError } = require("./middlewares/errors");
+const tuitsRouter = require("./routers/tuitsRouter");
 
 const app = express();
 
@@ -12,8 +12,9 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
 
+app.use("/", tuitsRouter);
+
 app.use(notFoundError);
 app.use(generalError);
 
 module.exports = app;
-
