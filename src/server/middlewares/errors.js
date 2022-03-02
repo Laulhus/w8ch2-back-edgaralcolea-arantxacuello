27ec3f81f0ba7a9761a60ea036930ce9728f0516
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-02 19:25:55 +0100

    error tests passed

--- src/server/middlewares/errors.js
+++ src/server/middlewares/errors.js
@@ -2,14 +2,18 @@ const chalk = require("chalk");
 const debug = require("debug");
 
 const notFoundError = (req, res) => {
-  res.status(404).json({ error: true, message: "Endpoint not found" });
+  debug(chalk.red("Endpoint not found!"));
+  res.status(404);
+  res.json({ error: true, message: "Endpoint not found!" });
 };
 
+// eslint-disable-next-line no-unused-vars
 const generalError = (err, req, res, next) => {
   debug(chalk.red(`Error: ${err.message}`));
   const errorCode = err.code ?? 500;
-  const errorMessage = err.code ? err.message : "General error";
-  res.status(errorCode).json({ error: true, message: errorMessage });
+  const errorMessage = err.code ? err.message : "Internal server error!";
+  res.status(errorCode);
+  res.json({ error: true, message: errorMessage });
 };
 
 module.exports = {
