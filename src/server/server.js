ce1857905278bfbb5a973a9c9cece43595040c13
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-02 19:12:11 +0100

    server function added and .env

---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const debug = require("debug")("tuits:server");
+
+const socialServidor = (port, app) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(`Server listening on http://localhost:${port}`);
+      resolve();
+    });
+
+    server.on("error", (error) => {
+      const errorMessage = `Couldn't start the server.${
+        error.code === "EADDRINUSE" ? ` Port ${port} busy` : ""
+      }`;
+      reject(new Error(errorMessage));
+    });
+  });
+
+module.exports = socialServidor;
